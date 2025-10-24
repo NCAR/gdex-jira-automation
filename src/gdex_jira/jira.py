1d6f70b6819020c36c5d1d8baab189c682f059d9
Riley Conroy <rpconroy@ucar.edu>
2025-10-24 09:57:04 -0600

    initial commit

---
+++ src/gdex_jira/jira.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+import os
+import requests
+from jira import JIRA
+
+
+def get_unassigned_tickets():
+    pass
+
+def change_status(ticket, status):
+    pass
+
+
