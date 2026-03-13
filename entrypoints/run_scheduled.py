0aebda01b6f898378829e6c07316b41d479da800
Calie Payne <caliepayne@ucar.edu>
2026-03-13 11:30:04 -0600

    Restructured scheduled job to use entrypoint -> workflow

---
+++ entrypoints/run_scheduled.py
@@ -0,0 +1,10 @@
+#Intialize Jira client and run scheduled tasks
+from jira_client import GdexJiraAutomator as JiraAuto
+from workflows.scheduled.ticket_assignment import assign_unassigned_tickets
+
+def main():
+    jira = JiraAuto(dry_run = True)
+    assign_unassigned_tickets(jira)
+
+if __name__ == "__main__":
+    main()
