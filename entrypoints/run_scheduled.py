5e515ab69feb9cfea21a3048403c2d9814fbd434
Cal <caliepayne@ucar.edu>
2026-03-13 12:23:39 -0600

    Initialize Jira client and update sys.path
    Initialize Jira client and add root directory to sys.path.

--- entrypoints/run_scheduled.py
+++ entrypoints/run_scheduled.py
@@ -1,5 +1,9 @@
 #Intialize Jira client and run scheduled tasks
 #from jira_client.helpers import GdexJiraAutomator as JiraAuto
+import sys
+import os
+# Add the root directory (gdex-jira-automation) to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from workflows.scheduled.ticket_assignment import assign_unassigned_tickets
 
 def main():
