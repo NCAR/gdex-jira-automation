4d442f0e6a3722ed9a65cd3492aee9f2f1d2ec42
Cal <caliepayne@ucar.edu>
2026-03-16 16:58:51 -0600

    Update run_scheduled.py

--- entrypoints/run_scheduled.py
+++ entrypoints/run_scheduled.py
@@ -3,8 +3,8 @@ from jira_client.helpers import GdexJiraAutomator as JiraAuto
 from workflows.scheduled.ticket_assignment import assign_unassigned_tickets
 
 def main():
-    jira = JiraAuto(dry_run = True)
-    assign_unassigned_tickets(False)
+    jira = JiraAuto(dry_run = False)
+    assign_unassigned_tickets(jira)
 
 if __name__ == "__main__":
     main()
