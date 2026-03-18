2ad1a6a50fd7f65ae6a2de2b49ed1969ed946aef
Cal <caliepayne@ucar.edu>
2026-03-18 15:51:47 -0600

    Update run_event.py

--- entrypoints/run_event.py
+++ entrypoints/run_event.py
@@ -6,18 +6,17 @@ from jira import JIRA, JIRAError
 # from workflows.scheduled.ticket_assignment import assign_unassigned_tickets
 
 def main():
-    # Load Jira Payload env variables
+# Load Jira Payload env variables
     incoming_ticket_id = os.getenv("PAYLOAD_TICKET_ID")
-    print("TESTING", incoming_ticket_id)
-    #Intialize Jira client
+
+#Intialize Jira client
     config_files = ["config/jira_config.yaml", "triager/workflow-mapping.yaml"]
     jira = JiraAuto(dry_run = True, config= config_files)
     
-    #Grab ticket contents from Jira API and add to payload
+#Grab ticket contents from Jira API and add to payload
     incoming_ticket_details = jira.get_unassigned_tickets(ticket_id = incoming_ticket_id)
-    # print(f"Incoming ticket payload: {incoming_ticket_details}")
 
-    # triage ticket based on customer request type
+# triage ticket based on customer request type
     triage_tickets(ticket_details = incoming_ticket_details, 
                    mapping_config = jira.triager_workflow, 
                    jira_object = jira)
