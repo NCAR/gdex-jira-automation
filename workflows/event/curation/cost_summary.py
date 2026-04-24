cd63991188c17f254758140dafbe32a6b2fc4d3b
Calie Payne <caliepayne@ucar.edu>
2026-04-24 12:12:45 -0600

    Added generate_cost_summary test function

--- workflows/event/curation/cost_summary.py
+++ workflows/event/curation/cost_summary.py
@@ -1,3 +1,5 @@
+from jira_client.helpers import GdexJiraAutomator as JiraAuto
 
-def generate_cost_summary ():
-    print ("GENERATE COST SUMMARY")
+def generate_cost_summary (jira_instance, ticket_id):
+    message = f"GENERATE COST SUMMARY for {ticket_id}"
+    JiraAuto.add_comment_to_ticket(ticket_id, message)
