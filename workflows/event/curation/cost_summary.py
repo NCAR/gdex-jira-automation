8b5108f8a2e57218dfceb6b0a08fc8ea91a106c3
Calie Payne <caliepayne@ucar.edu>
2026-04-24 12:20:11 -0600

    Fixed add_comment args in cost_summary

--- workflows/event/curation/cost_summary.py
+++ workflows/event/curation/cost_summary.py
@@ -2,4 +2,4 @@ from jira_client.helpers import GdexJiraAutomator as JiraAuto
 
 def generate_cost_summary (jira_instance, ticket_id):
     message = f"GENERATE COST SUMMARY for {ticket_id}"
-    JiraAuto.add_comment_to_ticket(ticket_id, message)
+    JiraAuto.add_comment_to_ticket(comment=message, ticket_id=ticket_id)
