3c0091a1940c6a29166151d4c20fcd55bf2cddd8
Calie Payne <caliepayne@ucar.edu>
2026-05-18 13:39:11 -0600

    international tickets move to low priority

--- workflows/event/services/ticket_priority.py
+++ workflows/event/services/ticket_priority.py
@@ -2,12 +2,12 @@ def prioritize_incoming_ticket(jira_instance, ticket_details):
     ticket_id = ticket_details['key']
     reporter_email = ticket_details["reporter_email"]
     international_tlds = {".uk", ".jp", ".de", ".fr", ".cn", ".au", ".ca"}
+    issue = jira_instance.jira.issue(ticket_id)
 
     # Check if the reporter's email contains any international TLD
     if any(reporter_email.endswith(tld) for tld in international_tlds):
-        return
+        issue.update(fields={"priority": {"name": "Low"}})
 
     if reporter_email.endswith(".edu"):
-        issue = jira_instance.jira.issue(ticket_id)
         issue.update(fields={"priority": {"name": "High"}})
         
